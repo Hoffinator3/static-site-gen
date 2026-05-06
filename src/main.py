76cad6e7ceebf873b7226405303e5a31927b654e
Hoffinator3 <123334411+Hoffinator3@users.noreply.github.com>
2026-05-06 17:29:49 -0400

    added src files

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print(f"hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
+
+
+main()
