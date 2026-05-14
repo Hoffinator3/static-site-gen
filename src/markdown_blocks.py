9057a1cecc78f11eaebbd4da0bb44f3e305830c2
Hoffinator3 <123334411+Hoffinator3@users.noreply.github.com>
2026-05-14 17:16:52 -0400

    added block files

---
+++ src/markdown_blocks.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def markdown_to_blocks(markdown):
+    filtered_blocks = []
+    blocks = markdown.split("\n\n")
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
